d42a0e84c79d88beec6babb3ea3a1393dd290f8a
Nafeez Ahamed <88371174+Heynafeezhere@users.noreply.github.com>
2026-09-16 13:05:03 -0700

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character,Character> closeToOpen = new HashMap<>();
+
+        closeToOpen.put(')','(');
+        closeToOpen.put('}','{');
+        closeToOpen.put(']','[');
+
+        for(char i : s.toCharArray()){
+            if(closeToOpen.containsKey(i)){
+                if(!stack.empty() && stack.peek() == closeToOpen.get(i)){
+                    stack.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+            else{
+                stack.push(i);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
